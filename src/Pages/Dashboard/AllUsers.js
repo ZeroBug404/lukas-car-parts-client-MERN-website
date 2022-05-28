e1cee21cae7a27296f56e0fbf92051a67d606f93
ZeroBug <404.zerobug@gmail.com>
2022-05-28 11:16:58 +0600

    created manage all orders page for admin

--- src/Pages/Dashboard/AllUsers.js
+++ src/Pages/Dashboard/AllUsers.js
@@ -1,53 +1,53 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { toast } from 'react-toastify';
-import auth from '../../firebase.init';
-import DeleteOrderConfirm from './DeleteOrderConfirm';
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { toast } from "react-toastify";
+import auth from "../../firebase.init";
 
 const AllUsers = () => {
-    const [user] = useAuthState(auth);
-    const [users, setUsers] = useState([]);
+  const [user] = useAuthState(auth);
+  const [users, setUsers] = useState([]);
 
-    // console.log(user);
-    useEffect(() => {
-        fetch(`http://localhost:5000/users`, {
-            method: 'GET',
-            headers: {
-                authorization: `Bearer ${localStorage.getItem('accessToken')}`
-            }
-        })
-        .then(res => res.json())
-        .then(data => {
-            setUsers(data);
-        })
-    },[users])
+  // console.log(user);
+  useEffect(() => {
+    fetch(`http://localhost:5000/users`, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, [users]);
 
-    const makeAdmin = (email) => {
-        fetch(`http://localhost:5000/users/admin/${email}`, {
-            method: 'PUT',
-            headers: {
-                authorization: `Bearer ${localStorage.getItem('accessToken')}`
-            }
-        })
-        .then(res => {
-            if(res.status === 403){
-                toast.error('Failed to make ann admin')
-            }
-            return res.json()})
-        .then(data => {
-            console.log(data);
+  const makeAdmin = (email) => {
+    fetch(`http://localhost:5000/users/admin/${email}`, {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => {
+        if (res.status === 403) {
+          toast.error("Failed to make ann admin");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
 
-            if(data.modifiedCount > 0){
-                toast.success('Successfully made an admin');
-            }
-        })
-    }
-    return (
-        <div>
-            <h2 className='text-3xl text-slate-600 font-semibold'>All User</h2>
+        if (data.modifiedCount > 0) {
+          toast.success("Successfully made an admin");
+        }
+      });
+  };
+  return (
+    <div>
+      <h2 className="text-3xl text-slate-600 font-semibold">All User</h2>
 
-            <div class="overflow-x-auto">
+      <div class="overflow-x-auto">
         <table class="table w-full">
           {/* <!-- head --> */}
           <thead>
@@ -58,12 +58,7 @@ const AllUsers = () => {
               <th>Action</th>
             </tr>
           </thead>
-          {/* {
-              users?.map(order => <DeleteOrderConfirm
-                key={order._id} 
-                order={order}
-                ></DeleteOrderConfirm>)
-          } */}
+
           <tbody>
             {users?.map((user, index) => (
               <tr key={user._id} className="text-slate-800">
@@ -72,20 +67,25 @@ const AllUsers = () => {
                 <td>{user.name}</td>
                 <td>{user.email}</td>
                 <td>
-                  {
-                      user.role !== 'admin' && <><label onClick={() => makeAdmin(user.email)} class="btn btn-success mr-2 btn-sm">
-                      make admin
+                  {user.role !== "admin" && (
+                    <>
+                      <label
+                        onClick={() => makeAdmin(user.email)}
+                        class="btn btn-success mr-2 btn-sm"
+                      >
+                        make admin
                       </label>
-                        <button class="btn btn-error btn-sm">delete</button></>
-                  }
+                      <button class="btn btn-error btn-sm">delete</button>
+                    </>
+                  )}
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default AllUsers;
--- src/Pages/Dashboard/ManageOrders.js
+++ src/Pages/Dashboard/ManageOrders.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const ManageOrders = () => {
-    return (
-        <div>
-            <h2 className="text-4xl">Manage all orders</h2>
-        </div>
-    );
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/orders`)
+      .then((res) => res.json())
+      .then((data) => {
+        setOrders(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h2 className="text-3xl text-slate-600 font-semibold">All Orders</h2>
+
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          {/* <!-- head --> */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Product Name</th>
+              <th>User Email</th>
+              <th>Quantity</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {orders?.map((order, index) => (
+              <tr key={order._id} className="text-slate-800">
+                <th>{index + 1}</th>
+
+                <td>{order.productName}</td>
+                <td>{order.userEmail}</td>
+                <td>{order.productQuantity}</td>
+                <td>
+                  <label class="btn btn-error mr-2 btn-sm">delete</label>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageOrders;
